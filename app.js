27768e8160c506bd11ef32dbc50b34233d3c19b0
nicola00 <nicolajobs00@gmail.com>
2023-02-02 15:13:10 +0100

    START

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('ciao tutti')
+
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero
+// e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi (quindi multipli di 15) “FizzBuzz”.
